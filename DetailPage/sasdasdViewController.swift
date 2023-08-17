eb18aa5214d344c295e5dacba1b94307419f8176
KangJiHun1028 <136081642+KangJiHun1028@users.noreply.github.com>
2023-08-17 14:10:53 +0900

    디테일 페이지

---
+++ DetailPage/sasdasdViewController.swift
@@ -0,0 +1,20 @@
+//
+//  sasdasdViewController.swift
+//  naverBandCopy
+//
+//  Created by t2023-m0053 on 2023/08/16.
+//
+
+import UIKit
+
+class sasdasdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
